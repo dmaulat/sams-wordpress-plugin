c535654db4bef290a0d780cf15df3f88d803b0c1
David Maulat <dmaulat@users.noreply.github.com>
2024-08-13 15:08:39 +0200

    add verband parameter to table shortcode

--- wordpress/shortcodes/sams_table_shortcode.php
+++ wordpress/shortcodes/sams_table_shortcode.php
@@ -5,10 +5,11 @@ use SAMSPlugin\SAMSTableRenderer;
 
 function fetch_and_render_sams_table( $atts ) {
     $a = shortcode_atts( array(
+        'verband' => 'bundesliga',
         'apikey' => 'asd',
         'matchseriesid' => '21416892'
     ), $atts );
 
-    return SAMSTableRenderer::fetchAndRender($a['apikey'], $a['matchseriesid']);
+    return SAMSTableRenderer::fetchAndRender($a['verband'], $a['apikey'], $a['matchseriesid']);
 }
 ?>
