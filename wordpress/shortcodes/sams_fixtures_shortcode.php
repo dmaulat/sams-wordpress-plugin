8429851e0e6c50f069d7144d91dd5fc847add465
David Maulat <dmaulat@users.noreply.github.com>
2024-08-13 15:06:19 +0200

    add verband to fixtures shortcode

--- wordpress/shortcodes/sams_fixtures_shortcode.php
+++ wordpress/shortcodes/sams_fixtures_shortcode.php
@@ -5,10 +5,11 @@ use SAMSPlugin\SAMSFixturesRenderer;
 
 function fetch_and_render_sams_fixtures( $atts ) {
     $a = shortcode_atts( array(
+        'verband' => 'bundesliga',
         'apikey' => 'asd',
         'matchseriesid' => '21416892',
         'teamid' => '21416933'
     ), $atts );
-    return SAMSFixturesRenderer::fetchAndRender($a['apikey'], $a['matchseriesid'], $a['teamid']);
+    return SAMSFixturesRenderer::fetchAndRender($a['verband'], $a['apikey'], $a['matchseriesid'], $a['teamid']);
 }
 ?>
