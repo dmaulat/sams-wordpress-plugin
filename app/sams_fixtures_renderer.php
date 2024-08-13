8ebb6aae7dc21fc1dee5f8b3ae98726c200cd824
David Maulat <dmaulat@users.noreply.github.com>
2024-08-13 15:10:22 +0200

    Update sams_fixtures_renderer.php

--- app/sams_fixtures_renderer.php
+++ app/sams_fixtures_renderer.php
@@ -8,15 +8,15 @@ use SAMSPlugin\Presenters\FixturesPresenter;
 
 class SAMSFixturesRenderer {
 
-    public static function fetchAndRender($apiKey, $matchSeriesId, $teamId) {
-        $fixturesUri = new FixturesURI($apiKey, $matchSeriesId, $teamId);
+    public static function fetchAndRender($verband, $apiKey, $matchSeriesId, $teamId) {
+        $fixturesUri = new FixturesURI($verband, $apiKey, $matchSeriesId, $teamId);
         $fetcher = new XMLFetcher();
         $fetchedXml = $fetcher->fetch($fixturesUri->toString());
         if (\is_a($fetchedXml, "SimpleXMLElement")) {
             $fixtures = new SAMSFixtures($fetchedXml);
             return FixturesPresenter::render($fixtures);
         } else {
-            return "Fixtures not found (MatchSeriesID: $matchSeriesId - TeamID: $teamId API-Key: " . $apiKey . ")";
+            return "Fixtures not found (Verband: $verband, MatchSeriesID: $matchSeriesId, TeamID: $teamId, API-Key: $apiKey)";
         }
     }
 }
